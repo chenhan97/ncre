6414bd6663825278baa5f80f2493def3e983dc85
chenhan97 <chris.yuan.ece@gmail.com>
2021-12-06 17:08:20 +0000

    update main

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import transformers
-import torch
-import numpy as np
-import torch.nn as nn
-from model.re_m import RelationExtractor
-from utils import DataLoader
-tokenizer_class, pretrained_weights = transformers.BertTokenizer, 'bert-base-uncased'
-tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-x = ['a good place to stay .', 'where are you ?']
-a = tokenizer(x, padding=True, truncation=True, return_tensors="pt")
-print(a)
+import argparse
+import Trainer
 
-input = a["input_ids"]
-attention_mask = a["attention_mask"]
-model = RelationExtractor()
-model(input,[[[0,1],[2,3]],[[0,1],[2,3]]])
 
-'''dl = DataLoader.TrainDataLoader("corpus/pubmed/test0.json", 12, False)
-dl.next()
-m = nn.Sigmoid()
-loss = nn.BCELoss()
-input = torch.randn(3, requires_grad=True)
-target = torch.empty(3).random_(2)
-print(m(input), target)
-output = loss(m(input), target)'''
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_data', type=str, default='corpus/pubmed/train.json')
+    parser.add_argument('--save_model_dir', type=str, default='saved_model')
+    parser.add_argument('--num_iter', type=int, default=50)
+    parser.add_argument('--batch_size', type=int, default=16)
 
+    args = parser.parse_args()
+    trainer = Trainer.train(args)
+
+
+if __name__ == "__main__":
+    main()
